35dc46164372f3797b8064ecd2997701efeb5fb7
seba <seba.flores.ram@gmail.com>
2024-07-21 19:46:06 -0400

    Modulo de estudiantesv2

--- src/estudiantes/estudiantes.service.ts
+++ src/estudiantes/estudiantes.service.ts
@@ -9,6 +9,11 @@ export class EstudiantesService {
     
     crearEstudiante(estudiante:Estudiante ):void{
         estudiante.id = this.estudiantes.length + 1;
+        for(let i = 0; i < this.estudiantes.length; i++){
+            if(this.estudiantes[i].email == estudiante.email){
+                return console.log("Estudiante ingresado con este correo ya existe, porfavor ingresar uno nuevo");
+            }
+        }
         this.estudiantes.push(estudiante);
     }
 
